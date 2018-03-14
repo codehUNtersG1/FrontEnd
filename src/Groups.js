e9ff42044917ca297968a9131d9f0609fddd7eeb
giovanny albarracin <gfalbarracinr@unal.edu.co>
2018-03-14 07:08:46 +0000

    Con algunas rutas ya definidas

---
+++ src/Groups.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Header from './Header';
+
+class Groups extends Component {
+  
+  render() {
+    return (
+      <div>
+        <Header/>
+        <h1> Este es la página de grupos</h1>
+      </div>
+    );
+  }
+}
+
+export default Groups;
